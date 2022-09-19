6903bfbd4bbe8070d4a0710071a065b3657adb92
pedrocarr <eng.pedro.amb@gmail.com>
2022-09-19 14:40:03 -0300

    setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27017/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity()
+export class Lesson {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
