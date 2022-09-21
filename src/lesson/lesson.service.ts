b6b5db4f10bea8a050139b01317fa33151598ea8
pedrocarr <eng.pedro.amb@gmail.com>
2022-09-21 08:20:07 -0300

    assign students to Lesson

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
 import { v4 as uuid } from 'uuid';
 import { CreateLessonInput } from './lesson.input';
+import { Student } from 'src/student/student.entity';
 
 @Injectable()
 export class LessonService {
@@ -20,14 +21,26 @@ export class LessonService {
   }
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
-    const { name, startDate, endDate } = createLessonInput;
+    const { name, startDate, endDate, students } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
       startDate,
       endDate,
+      students,
     });
 
+    return this.lessonRepository.save(lesson);
+  }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOneByOrFail({
+      id: lessonId,
+    });
+    lesson.students = [...lesson.students, ...studentIds];
     return this.lessonRepository.save(lesson);
   }
 }
