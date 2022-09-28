03f1c2d1cefc3fe188237c397ab49c60497fb145
pedrocarr <eng.pedro.amb@gmail.com>
2022-09-28 11:09:08 -0300

    create port

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
